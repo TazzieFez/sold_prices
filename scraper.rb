9be6a1a78ecd1962b42c743dfa50e298975b85a9
Rob Tedge <rjtedge@users.noreply.github.com>
2017-08-16 12:27:53 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -23,3 +23,25 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+require 'mechanize'
+require 'scraperwiki'
+
+agent = Mechanize.new
+url = 'https://morph.io/documentation/examples/australian_members_of_parliament'
+
+["1", "2", "3"].each do |page_number|
+  page = agent.get(url + "?page=" + page_number)
+
+  page.at('.search-filter-results').search('li').each do |li|
+    member = {
+      title: li.at('.title').inner_text.strip,
+      electorate: li.search('dd')[0].inner_text,
+      party: li.search('dd')[1].inner_text,
+      url: li.at('.title a').attr('href')
+    }
+
+    p member
+    ScraperWiki.save_sqlite([:title], member)
+  end
+end
